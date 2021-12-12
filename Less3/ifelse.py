d19b835fd2d1ac24d58a4d8aed216f38270e226f
YurioMan <58913375+YurioMan@users.noreply.github.com>
2021-12-12 18:49:20 +0300

    Update ifelse.py

--- Less3/ifelse.py
+++ Less3/ifelse.py
@@ -1,1 +1,11 @@
-
+a = int (input ('a = '))
+b = int (input ('b = '))
+if b > 0:
+    print('a : b = ', a/b)
+else:
+    print('Chislo ne sootvetstvuet')
+    b = int (input ('Vvedite chislo b : '))
+    if b == 0:
+        print (' vi apple')
+    else:
+        print('a : b = ', a/b)
