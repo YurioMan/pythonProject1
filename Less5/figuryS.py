2928a9e22cf532b876c466934ba7fd4ee3ac53b5
YurioMan <58913375+YurioMan@users.noreply.github.com>
2021-12-12 18:59:58 +0300

    Update figuryS.py

--- Less5/figuryS.py
+++ Less5/figuryS.py
@@ -1,1 +1,19 @@
+xs = input ()
 
+if xs == "треугольник":
+    a = float(input())
+    b = float(input())
+    c = float(input())
+    p = (a + b + c)/2
+    S = (p*(p-a)*(p-b)*(p-c))**0.5
+    
+if xs == "прямоугольник":
+    a = float(input())
+    b = float(input())
+    S = a * b
+    
+if xs == "круг":
+    r = float(input())
+    i = 3.14
+    S = i * r**2
+print (S)
