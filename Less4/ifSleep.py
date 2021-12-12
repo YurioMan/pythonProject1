e2de524e9e587ff19c574d01570beb07bf368251
YurioMan <58913375+YurioMan@users.noreply.github.com>
2021-12-12 18:57:33 +0300

    Update ifSleep.py

--- Less4/ifSleep.py
+++ Less4/ifSleep.py
@@ -1,1 +1,11 @@
-
+A = int(input())
+B = int(input())
+H = int(input())
+if A <= H <= B:
+    print('Это нормально') 
+else:
+        if H < A:
+            print('Недосып')
+        else:
+            if H > B:
+                print ('Пересып')
