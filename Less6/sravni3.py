87083dcd7bc9606557af7506bc3ee8293ea9d82b
YurioMan <58913375+YurioMan@users.noreply.github.com>
2021-12-12 19:03:56 +0300

    Update sravni3.py

--- Less6/sravni3.py
+++ Less6/sravni3.py
@@ -1,1 +1,17 @@
-
+a, b, c = int(input()), int(input()), int(input())
+if (a >=b) and (a >=c):
+    xmax = a
+if (b >= a) and (b >= c):
+    xmax = b
+if (c >= a) and (c >= b):
+    xmax = c
+    
+if (a <= b) and (a <= c):
+    xmin = a
+if (b <= a) and (b <= c):
+    xmin = b
+if (c <= a) and (c <= b):
+    xmin = c
+midl = a+b+c-xmax-xmin
+ 
+print ( xmax,xmin,midl, sep='\n')
