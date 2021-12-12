5831883508be3e6a9bc234dcbd524fc5e47fbb1d
YurioMan <58913375+YurioMan@users.noreply.github.com>
2021-12-12 18:53:31 +0300

    Update calcul.py

--- Less3/calcul.py
+++ Less3/calcul.py
@@ -1,1 +1,29 @@
+a = float(input())
+b = float(input())
+oper = input()
+if oper == '+':
+  print (a + b)
+if oper == '-':
+  print(a -b)
+if oper == '*':
+  print(a * b)
+if oper == '/':
+    if b!=0:
+          print (a / b)
+    else:
+        print('Деление на 0!')
+        
+if oper == 'mod':
+    if b!=0:
+          print (a % b)
+    else:
+        print('Деление на 0!')
+  
+if oper =='pow':
+    print (a**b)
+if oper == 'div':
+    if b!=0:
+          print (a // b)
+    else:
+        print('Деление на 0!')
 
